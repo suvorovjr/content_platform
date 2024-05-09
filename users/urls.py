83f8ec303c1ed673bbeccf4270e837dd3bfe42f1
suvorovjr <pglavan1998@mail.ru>
2024-05-09 22:13:16 +0300

    add new URLS

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from .views import IndexView
+from .views import IndexView, UserCreateView, LoginView, ConfirmCodeView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('login/', UserCreateView.as_view(), name='login'),
+    path('signin/', LoginView.as_view(), name='signin'),
+    path('confirm_code/', ConfirmCodeView.as_view(), name='confirm_code'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
