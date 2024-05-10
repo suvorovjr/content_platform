8a2c65319273574d2015f27a490f7e90c4361ada
suvorovjr <pglavan1998@mail.ru>
2024-05-10 23:41:24 +0300

    add Post admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_paid_content')
