4851ed566f2bbd15e6ed5ee1d1c53be7bf1cb857
suvorovjr <pglavan1998@mail.ru>
2024-05-10 09:53:45 +0300

    add change in blogger list

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from .views import IndexView, UserCreateView, LoginView, ConfirmCodeView
+from .views import IndexView, UserCreateView, LoginView, ConfirmCodeView, UserListView
 
 app_name = UsersConfig.name
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('signin/', LoginView.as_view(), name='signin'),
     path('confirm_code/', ConfirmCodeView.as_view(), name='confirm_code'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('blogger/list/', LogoutView.as_view(), name='list'),
+    path('blogger/list/', UserListView.as_view(), name='list'),
 ]
