1b095adc04b9392eeb8883e6cf021f30931cc402
suvorovjr <pglavan1998@mail.ru>
2024-05-10 20:19:46 +0300

    add Post model

--- content/models.py
+++ content/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=155, verbose_name='Заголовок')
+    body = models.TextField(verbose_name='Информациионный блок')
+    imagine = models.ImageField(upload_to='post_images/', verbose_name='Облложка')
+    is_paid_content = models.BooleanField(verbose_name='Флаг платной статьи')
+
+    def __str__(self):
+        return f'{self.title} - {self.is_paid_content}'
+
+    class Meta:
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
