2de43c5679fa4b295e72b29ba40ce29f35159e2a
suvorovjr <pglavan1998@mail.ru>
2024-05-13 00:27:11 +0300

    add AbstractPublication and Video models

--- content/admin.py
+++ content/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Video
 
 
 @admin.register(Post)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_paid_content', 'author')
+
+
+@admin.register(Video)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_paid_content', 'author')
--- content/models.py
+++ content/models.py
@@ -2,16 +2,33 @@ from django.db import models
 from users.models import User, NULLABLE
 
 
-class Post(models.Model):
+class AbstractPublication(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
     title = models.CharField(max_length=155, verbose_name='Заголовок')
     body = models.TextField(verbose_name='Информациионный блок')
+    video = models.FileField(upload_to='videos/', verbose_name='Видео')
     imagine = models.ImageField(upload_to='post_images/', verbose_name='Облложка', **NULLABLE)
     is_paid_content = models.BooleanField(verbose_name='Флаг платной статьи')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.title} - {self.is_paid_content}'
 
     class Meta:
+        abstract = True
+
+
+class Post(AbstractPublication):
+    video = None
+
+    class Meta:
         verbose_name = 'статья'
         verbose_name_plural = 'статьи'
+
+
+class Video(AbstractPublication):
+    body = None
+
+    class Meta:
+        verbose_name = 'видео'
+        verbose_name_plural = 'видео'
