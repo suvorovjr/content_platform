7c3f8deff35ca82f44b95c27b2ddacbec6d182de
suvorovjr <pglavan1998@mail.ru>
2024-05-12 23:50:11 +0300

    add changes Post create view

--- content/views.py
+++ content/views.py
@@ -7,3 +7,15 @@ class PostCreateView(CreateView):
     form_class = PostForm
     success_url = reverse_lazy('users:index')
     template_name = 'content/post_form.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = 'Создание поста'
+        return context_data
+
+    def form_valid(self, form):
+        if form.is_valid():
+            post = form.save()
+            post.author = self.request.user
+            post.author.save()
+        return super().form_valid(form)
