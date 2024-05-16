d5221f8f8c9a60915509884fe7aaafc330a723c6
suvorovjr <pglavan1998@mail.ru>
2024-05-16 20:44:06 +0300

    add Subscription model

--- payment/models.py
+++ payment/models.py
@@ -1,14 +1,17 @@
+import uuid
 from django.db import models
+from users.models import User, Author, NULLABLE
 
-# class Subscription(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, verbose_name='Пользователь')
-#     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='subscription_to', verbose_name='Автор')
-#     is_active = models.BooleanField(default=True, verbose_name='Признак подписки')
-#
-#     def __str__(self):
-#         return f'{self.user} {"подписан" if self.is_active else "не подписан"} {self.author}'
-#
-#     class Meta:
-#         verbose_name = 'подписка'
-#         verbose_name_plural = 'подписки'
+
+class Subscription(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE, )
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='subscription_to', verbose_name='Автор')
+    is_active = models.BooleanField(default=True, verbose_name='Признак подписки')
+
+    def __str__(self):
+        return f'{self.user} {"подписан" if self.is_active else "не подписан"} {self.author}'
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
