863ca3bf0e93b7f50ce29c650297f1968a4cd35f
suvorovjr <pglavan1998@mail.ru>
2024-05-17 22:49:32 +0300

    add payment admin panel

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Subscription
+from .models import Subscription, Payment
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'author', 'is_active')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author', 'stripe_customer_id', 'is_paid')
