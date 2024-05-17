62e0c674fe2ac24387803d13fdae31a957b41cf7
suvorovjr <pglavan1998@mail.ru>
2024-05-17 20:02:38 +0300

    add payment create view

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from payment.apps import PaymentConfig
-from .views import SubscribeToAuthor
+from .views import SubscribeToAuthor, PaymentCreateView
 
 app_name = PaymentConfig.name
 
 urlpatterns = [
     path('subscribe-author/', SubscribeToAuthor.as_view(), name='subscribe_author'),
+    path('pay/', PaymentCreateView.as_view(), name='pay'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
 from django.views import View
-from .models import Subscription
+from django.conf import settings
+from .models import Subscription, Payment
+from .services import get_payment
 from users.models import Author
 
 
@@ -21,3 +22,22 @@ class SubscribeToAuthor(View):
                 subscription.is_active = True
             subscription.save()
         return redirect(request.META['HTTP_REFERER'])
+
+
+class PaymentCreateView(View):
+    def post(self, request, *args, **kwargs):
+        author_id = self.request.POST.get('author_id', None)
+        user = self.request.user
+        author = Author.objects.get(id=author_id)
+        if not Payment.objects.filter(user=user, author=author, is_paid=False).exists():
+            new_payment = get_payment(settings.STRIPE_API_KEY, author)
+            payment = Payment.objects.create(
+                user=user,
+                author=author,
+                stripe_subscription_id=new_payment.get('stripe_payment_id'),
+                stripe_url=new_payment.get('stripe_url')
+            )
+            payment.save()
+        else:
+            payment = Payment.objects.get(user=user, author=author, is_paid=False)
+        return redirect(payment.stripe_url)
