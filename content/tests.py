dcceb307b829c4680df358c88d351126588413d7
suvorovjr <pglavan1998@mail.ru>
2024-05-18 05:07:50 +0300

    add test content feed

--- content/tests.py
+++ content/tests.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import PermissionDenied
 from django.test import TestCase, RequestFactory
-from .views import PostCreateView
+from .views import PostCreateView, FeedListView
 from users.models import User
 
 
@@ -10,7 +11,6 @@ class PostCreateViewTests(TestCase):
         self.factory = RequestFactory()
         self.user = User.objects.create(phone_number='987654310')
         self.author_user = User.objects.create(phone_number='987654311', is_author=True)
-        self.view = PostCreateView
 
     def test_view_with_author(self):
         request = self.factory.get('/content/post/create')
@@ -23,3 +23,21 @@ class PostCreateViewTests(TestCase):
         request.user = self.user
         with self.assertRaises(PermissionDenied):
             PostCreateView.as_view()(request)
+
+
+class FeedListViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/content/feed/')
+        request.user = self.user
+        response = FeedListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/content/feed/')
+        request.user = AnonymousUser()
+        response = FeedListView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
