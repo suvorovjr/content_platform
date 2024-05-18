47ee461f2b585ec1b11eb06eccf5f64c44850b22
suvorovjr <pglavan1998@mail.ru>
2024-05-18 05:13:33 +0300

    add UserSubscriptionList test

--- payment/tests.py
+++ payment/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import PermissionDenied
+from django.test import TestCase, RequestFactory
+from .views import UserSubscriptionList
+from users.models import User
 
-# Create your tests here.
+
+class UserSubscriptionListTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+
+    def test_view_with_author(self):
+        request = self.factory.get('/content/post/create')
+        request.user = self.user
+        response = UserSubscriptionList.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_with_non_author(self):
+        request = self.factory.get('/content/post/create')
+        request.user = AnonymousUser()
+        response = UserSubscriptionList.as_view()(request)
+        self.assertEqual(response.status_code, 302)
