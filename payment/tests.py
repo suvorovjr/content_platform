9ad4229679ebe5775288b456404487c05d64d6a3
suvorovjr <pglavan1998@mail.ru>
2024-05-19 16:01:07 +0300

    add changes tests payment and users app

--- payment/tests.py
+++ payment/tests.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AnonymousUser
-from django.core.exceptions import PermissionDenied
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, Client
+from django.urls import reverse
+from .models import Subscription
 from .views import UserSubscriptionList
-from users.models import User
+from users.models import User, Author
 
 
 class UserSubscriptionListTests(TestCase):
@@ -12,13 +13,34 @@ class UserSubscriptionListTests(TestCase):
         self.user = User.objects.create(phone_number='987654310')
 
     def test_view_with_author(self):
-        request = self.factory.get('/content/post/create')
+        request = self.factory.get('/payment/subscribers-list')
         request.user = self.user
         response = UserSubscriptionList.as_view()(request)
         self.assertEqual(response.status_code, 200)
 
     def test_view_with_non_author(self):
-        request = self.factory.get('/content/post/create')
+        request = self.factory.get('/payment/subscribers-list')
         request.user = AnonymousUser()
         response = UserSubscriptionList.as_view()(request)
         self.assertEqual(response.status_code, 302)
+
+# class SubscribeToAuthorTest(TestCase):
+#
+#     def setUp(self):
+#         self.user = User.objects.create_user(phone_number='9876543211', password='password')
+#         self.author_user = User.objects.create(phone_number='9876543210', password='password', is_author=True)
+#         self.author = Author.objects.create(
+#             user=self.author_user,
+#             blog_name='Test blogname',
+#             blog_description='Test description blog. Description for test.',
+#             subscription_price=2000
+#         )
+#         self.client = Client()
+#         self.url = reverse('payment:subscribe_author')
+#
+#     def test_subscribe_to_author_with_valid_author_id(self):
+#         data = {'author_id': self.author.id}
+#         self.client.login(username='9876543211', password='password')
+#         response = self.client.post(self.url, data)
+#         print(response)  # Чтобы увидеть содержимое ответа
+#         print(response.status_code)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,111 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import PermissionDenied
+from django.test import TestCase, RequestFactory
+from .models import User, Author
+from .views import UserCreateView, LoginView, ProfileView, AuthorProfileView, AuthorProfileUpdateView
 
-# Create your tests here.
+
+class UserCreateViewTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/login')
+        request.user = self.user
+        with self.assertRaises(PermissionDenied):
+            UserCreateView.as_view()(request)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/login')
+        request.user = AnonymousUser()
+        response = UserCreateView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginViewTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/signin')
+        request.user = self.user
+        with self.assertRaises(PermissionDenied):
+            LoginView.as_view()(request)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/signin')
+        request.user = AnonymousUser()
+        response = LoginView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class ProfileViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/profile')
+        request.user = self.user
+        response = ProfileView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/profile')
+        request.user = AnonymousUser()
+        response = ProfileView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
+
+
+class AuthorProfileViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+        self.author_user = User.objects.create(phone_number='987654311', is_author=True)
+        self.author = Author.objects.create(
+            user=self.author_user,
+            blog_name='Test blogname',
+            blog_description='Test description blog. Description for test.',
+            subscription_price=2000
+        )
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/author-profile')
+        request.user = self.user
+        with self.assertRaises(PermissionDenied):
+            AuthorProfileView.as_view()(request)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/author-profile')
+        request.user = self.author_user
+        response = AuthorProfileView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AuthorProfileUpdateViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(phone_number='987654310')
+        self.author_user = User.objects.create(phone_number='987654311', is_author=True)
+        self.author = Author.objects.create(
+            user=self.author_user,
+            blog_name='Test blogname',
+            blog_description='Test description blog. Description for test.',
+            subscription_price=2000
+        )
+
+    def test_view_with_authenticated_user(self):
+        request = self.factory.get('/author-profile/update')
+        request.user = self.user
+        with self.assertRaises(PermissionDenied):
+            AuthorProfileUpdateView.as_view()(request)
+
+    def test_view_with_non_authenticated_user(self):
+        request = self.factory.get('/author-profile/update')
+        request.user = self.author_user
+        response = AuthorProfileUpdateView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
